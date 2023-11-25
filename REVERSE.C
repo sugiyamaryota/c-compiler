16aa3f14a4745c860545c75fe0ff1bd3c58147b7
sugiyamaryota <guqeeky6va8@gmail.com>
2023-11-25 20:36:58 +0900

    REVERSE

---
+++ REVERSE.C
@@ -0,0 +1,12 @@
+#define NOCCARGC
+
+reverse(s) char *s; {
+    char *j;
+    int c;
+    j = s + strlen(s) - 1;
+    while(s < j) {
+        c = *s;
+        *s++ = *j;
+        *j-- = *c;
+    }
+}
