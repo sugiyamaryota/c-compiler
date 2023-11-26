fcd8038e896a13265ffc0493c6abf238e3eb697b
sugiyamaryota <guqeeky6va8@gmail.com>
2023-11-26 14:46:21 +0900

    UTOI

---
+++ UTOI.C
@@ -0,0 +1,12 @@
+#include stdio.h
+
+utoi(decstr, nbr) char *decstr; int *nbr; {
+    int d,t; d=0;
+    *nbr=0;
+    while((*decstr>='0')&(*decstr<='9')) {
+        t=*nbr;t=(10*t) + (*decstr++ - '0');
+        if((t>=0)&(*nbr<0)) return ERR;
+        d++; *nbr=t;
+    }
+    return d;
+}
