cac0a69361f6e3477eb8171f9c99fa21fb486af8
sugiyamaryota <guqeeky6va8@gmail.com>
2023-12-14 09:21:57 +0900

    atan2

---
+++ SMLCP1/PART1/ATAN2.C
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+#include "float.h"
+
+atan2(y,x)
+double x, y;
+{
+    double a;
+    if(fabs(x) >= fabs(y)) {
+        a = atan(y/x);
+        if(x < 0.0) {
+            if( y >= 0.0) a += pi;
+            else a -= pi;
+        }
+    }
+    else {
+        a = -atan(x/y);
+        if (y < 0.0) a -=halfpi;
+        else a += halfpi;
+    }
+    return a;
+}
