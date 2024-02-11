7f5e6073ffd49023da254606d2c77cdba87ab871
sugiyamaryota <guqeeky6va8@gmail.com>
2024-02-11 16:06:35 +0900

    zopt1

---
+++ SMLCP1/PART2/ZOPT1.C
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include "zopt.h"
+
+pass1()
+{
+    int i;
+    int saved[5];
+    char *temp;
+    char *tail,*tail2;
+    char *last,*this,*next;
+
+    switch_down(1)
+    saved[0] = saved[1] = saved[2] = saved[3] = saved[4] =0
+
+    last = alloc(LINELEN);
+    this = alloc(LINELEN);
+    next = alloc(LINELEN);
+
+    getline(last);
+    getline(this);
+
+    
+
+}
