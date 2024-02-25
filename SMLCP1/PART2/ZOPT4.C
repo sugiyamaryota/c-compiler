d510a2189c249d5fdc267b0e87c068a10eee9e73
sugiyamaryota <guqeeky6va8@gmail.com>
2024-02-25 13:44:54 +0900

    zopt4

--- SMLCP1/PART2/ZOPT4.C
+++ SMLCP1/PART2/ZOPT4.C
@@ -17,3 +17,34 @@ char *string, *token1, *token2;
     }
     *token2 = 0;
 }
+
+jumps(s1, s2, s3, save, last, this, next)
+char *s1;
+char *s2;
+char *s3;
+int *save;
+char **last;
+char **this;
+char **next;
+{
+    char line[LINELEN], *temp, *tail;
+
+    if( tail=match(s1,*last) ){
+        if(tail=match(tail,*next)){
+            if(*tail == ':'){
+                if(tail=match(s2, *this)){
+                    if(islower(*tail)){
+                        strcpy(line,s3)
+                        strcat(line,tail)
+                        strcpy(*last,line)
+                        temp = *this;
+                        *this = *next;
+                        *next = temp;
+                        p_read(*next);
+                        ++(*save);
+                    }
+                }
+            }
+        }
+    }
+}
