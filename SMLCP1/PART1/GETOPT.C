650f8153cf2d7b098fbb414bf3ca36c8dd722f3f
sugiyamaryota <guqeeky6va8@gmail.com>
2024-03-04 09:08:50 +0900

    getopt

---
+++ SMLCP1/PART1/GETOPT.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int optind = 1;
+char *optarg;
+
+char *argind = "";
+char nullch = 0;
+
+getopt(nargc, nargv, optstr)
+int nargc;
+char **nargv;
+char *optstr;
+{
+    
+}
