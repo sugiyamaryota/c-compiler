dfadd130de8af8477c9b018e5a3edfc5acb6d280
sugiyamaryota <guqeeky6va8@gmail.com>
2024-03-05 21:36:40 +0900

    getopt

--- SMLCP1/PART1/GETOPT.C
+++ SMLCP1/PART1/GETOPT.C
@@ -12,5 +12,46 @@ int nargc;
 char **nargv;
 char *optstr;
 {
-    
+    char *listind;
+    char optopt;
+
+    if(*argind == 0){
+        if(optind >= nargc) return(-1);
+            argind = nargv[optind];
+        if(*argind != '-') return(-1);
+        if(++argind == 0) return(-1);
+        if(*argind != '-') {
+            ++optind;
+            return(-1);
+        }
+    }
+
+    optopt = *argind++;
+    listind = strchr(optstr, optopt);
+    if(optopt == ':' || listind == 0) {
+        if(*argind == 0) ++optind;
+        puts("illegal option -- ");
+        putchar(optopt);
+        puts("\n");
+        return("?");
+    }
+    if(*++listind != ':'){
+        optarg = 0;
+        if(*argind == 0) ++optind;
+    }
+    else {
+        if (*argind) optarg = argind;
+        else if(nargc <= ++optind){
+            argind = &nullch;
+            puts("option requires an argument -- ");
+            putchar(optopt);
+            puts("\n");
+            return("?");
+        }
+        else optarg = nargv[optind];
+        argind = &nullch;
+        ++optind;
+    }
+    return(optopt);
+
 }
