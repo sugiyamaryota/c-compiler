21d005b1e0181517fe9b11afdecf781731482815
sugiyamaryota <guqeeky6va8@gmail.com>
2024-03-12 23:02:26 +0900

    plot

--- SMLCP1/PART1/PLOT.C
+++ SMLCP1/PART1/PLOT.C
@@ -14,7 +14,20 @@
 ;
     global qplot
 qplot: LD HL,7; put SP+7 in HL
-
+    ADD HL,SP
+    LD D,(HL)  ;fetch msb of x
+    DEC HL
+    LD E,(HL)  ;fetch lsb of x
+    DEC HL
+    DEC HL
+    LD A,(HL)  ;fetch y to A
+    CPL
+    DEC HL
+    DEC HL
+    LD H,(HL)
+    LD L,A  ;put -y in L
+    LD C,76 ;call plot RSX
+    JP 5    ;no need to sign extend
 ;
 ;
 #endasm
