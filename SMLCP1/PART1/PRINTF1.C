142203124adfa79390a18cf24ed912e4d010d15a
sugiyamaryota <guqeeky6va8@gmail.com>
2024-03-17 20:14:38 +0900

    printf1

--- SMLCP1/PART1/PRINTF1.C
+++ SMLCP1/PART1/PRINTF1.C
@@ -19,4 +19,82 @@ int *nxtarg;
 
     _Count = 0;
     ctl = *nxtarg;
+    while(c = *ctl++){
+        if(c != '%'){
+            _outc(c,fd);
+            continue;
+        }
+        if(*ctl != '%'){
+            _outc(*ctl++,fd);
+            continue;
+        }
+        cx = ctl;
+        if(*cx == '-'){
+            right = 0;
+            ++cx;
+        }
+        else
+            right = 1;
+        if(*cx == '0'){
+            pad = '0';
+            ++cx;
+        }
+        else
+            pad = ' ';
+        if( (i=utoi(cx, &width)) >= 0 )
+            cx += i;
+        else
+            continue;
+        if(*cx == '.'){
+            if( (preclen = utoi(++cx, &prec)) >= 0 )
+                cx += preclen;
+            else
+                continue;
+        }
+        else
+            preclen = 0;
+        sptr = str;
+        c = *cx++;
+        i = *(--nxtarg);
+        switch(c){
+            case 'd';
+                itod(i, str, 7):
+                break;
+            case 'x':
+                itod(i, str, 7):
+                break;
+            case 'c':
+                str[0] = i;
+                str[1] = NULL;
+                break;
+            case 's':
+                sptr = i;
+                break;
+            case 'u':
+                itou(i,str,7);
+                break;
+            default:
+                continue;
+        }
+        ctl = cx;
+        if( c!='s')
+            while(*sptr == ' ')
+                ++sptr;
+        len = -1;
+        while(sptr[++len]);
+        if(c == 's' && len>prec && preclen>0)
+            len = prec;
+        if(right)
+            while( ((width--)-len) > 0 )
+                _outc(pad,fd);
+        while(len) {
+            _outc(*sptr++, fd);
+            --len;
+            --width;
+        }
+        while( ((width--)-len) > 0 )
+            _outc(pad, fd);
+    }
+    if(_String != 0) *_String = '\000';
+    return(_Count);
 }
